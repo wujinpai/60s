bcbfc6b8054dcce9cceddb962014bb9ad39cf093
浮生若梦 <888454@gmail.com>
2026-05-14 18:42:07 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,11 @@ export const config = {
   host: process.env.HOST || '0.0.0.0',
   port: process.env.PORT ? +process.env.PORT : 4399,
   group: '595941841',
-  author: 'Viki <hi@viki.moe>',
-  github: 'https://github.com/vikiboss/60s',
+  author: '浮生若梦 <info@gii.cn>',
+  github: 'https://github.com/wujinpai/60s',
   debug: !!process.env.DEBUG,
   overseas_first: !!process.env.OVERSEAS_FIRST,
   encodingParamName: process.env.ENCODING_PARAM_NAME || 'encoding',
 }
 
-export const COMMON_MSG = `获取成功。当前部署平台 Deno Deploy Classic 预计将于 7 月 20 日 终止服务，公共 API 服务后续迁移到其他平台后，额度、兼容性等方面可能会受影响。建议尽快迁移至私有部署，以确保服务持续可用。开源地址 ${config.github}，反馈群 ${config.group}。`
+export const COMMON_MSG = `获取成功。当前服务由gii.cn提供`
